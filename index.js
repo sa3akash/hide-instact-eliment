42e0105f72a6e53e65a0d9c8b8cad250e4904163
Shakil Ahmed <92939397+sa3akash@users.noreply.github.com>
2022-10-28 19:45:05 +0600

    Update index.js

--- index.js
+++ index.js
@@ -22,3 +22,42 @@ document.onkeydown = function(e) {
      return false;
   }
 }
+
+
+/////////////////////////////////////
+///////////////////////////////////////////////////////////////
+/////////////////////////////////////
+react 
+
+
+
+
+  useEffect(() => {
+    typeof window !== undefined &&
+      window.document.addEventListener("contextmenu", (e) => {
+        e.preventDefault();
+      });
+  }, []);
+
+  document.onkeydown = function(e) {
+    console.log(e.key)
+    if(e.key === 'F12') {
+       return false;
+    }
+    if(e.ctrlKey && e.shiftKey && e.key === 'I') {
+       return false;
+    }
+    if(e.ctrlKey && e.shiftKey && e.key === 'C') {
+       return false;
+    }
+    if(e.ctrlKey && e.shiftKey && e.key === 'J') {
+       return false;
+    }
+    if(e.ctrlKey && e.key === 'u') {
+       return false;
+    }
+  }
+
+
+
+
